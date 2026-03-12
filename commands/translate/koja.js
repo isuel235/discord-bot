e6c33dc5bfecfce2a2a35bc17aa2ae86e4bd041c
isuel235 <min1028004@gmail.com>
2026-03-12 20:05:49 +0900

    change console log

--- commands/translate/koja.js
+++ commands/translate/koja.js
@@ -35,7 +35,7 @@ module.exports = {
 
             const data = await response.json();
 
-            console.log(data);
+            //console.log(data);
 
             const translated = data.message.result.translatedText;
 

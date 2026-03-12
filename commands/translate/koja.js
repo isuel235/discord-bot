c28e60ecb31e18a69537d38f2ee3f123c5fefe74
isuel235 <min1028004@gmail.com>
2026-03-12 15:39:18 +0900

    initial commit

---
+++ commands/translate/koja.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { X_NAVER_CLIENT_ID, X_NAVER_CLIENT_SECRET } = require('../../config.json');
+
+const data = new SlashCommandBuilder()
+    .setName('koja')
+    .setDescription('Translate Korean to Japanese.')
+    .addStringOption((option) => 
+        option
+            .setName('text')
+            .setDescription('Korean Text')
+            .setRequired(true)    
+    );
+
+module.exports = {
+    data,
+    async execute(interaction) {
+
+        const text = interaction.options.getString('text');
+
+        try {
+
+            const response = await fetch("https://papago.apigw.ntruss.com/nmt/v1/translation", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "x-ncp-apigw-api-key-id": X_NAVER_CLIENT_ID,
+                    "x-ncp-apigw-api-key": X_NAVER_CLIENT_SECRET
+                },
+                body: new URLSearchParams({
+                    source: "ko",
+                    target: "ja",
+                    text: text
+                })
+            });
+
+            const data = await response.json();
+
+            console.log(data);
+
+            const translated = data.message.result.translatedText;
+
+            await interaction.reply(`${text} >> ${translated}`);
+
+        } catch (error) {
+            console.error(error);
+            await interaction.reply("Error");
+        }
+
+    },
+};

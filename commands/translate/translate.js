fd20d23795e3c207b74956cc300389daffee7b9b
isuel235 <min1028004@gmail.com>
2026-03-19 02:04:26 +0900

    change translate to detect language

--- commands/translate/translate.js
+++ commands/translate/translate.js
@@ -12,18 +12,6 @@ const data = new SlashCommandBuilder()
     )
     .addStringOption((option) =>
         option
-            .setName('src')
-            .setDescription("Source language")
-            .setRequired(true)
-            .addChoices(
-                { name: 'Korean', value: 'ko'},
-                { name: 'Japanese', value: 'ja'},
-                { name: 'English', value: 'en'},
-                { name: 'Chinese', value: 'zh-CN'}
-            )
-    )
-    .addStringOption((option) =>
-        option
             .setName('dest')
             .setDescription("Destination language")
             .setRequired(true)
@@ -40,11 +28,26 @@ module.exports = {
     async execute(interaction) {
 
         const text = interaction.options.getString('text');
-        const src = interaction.options.getString('src');
         const dest = interaction.options.getString('dest');
 
-        try{
-            const response = await fetch("https://papago.apigw.ntruss.com/nmt/v1/translation", {
+        try {
+
+            const response = await fetch("https://papago.apigw.ntruss.com/langs/v1/dect", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "x-ncp-apigw-api-key-id": X_NAVER_CLIENT_ID,
+                    "x-ncp-apigw-api-key": X_NAVER_CLIENT_SECRET
+                },
+                body: new URLSearchParams({
+                    query: text
+                })
+            });
+
+            const data = await response.json();
+            const src = data.langCode;
+
+            const trans = await fetch("https://papago.apigw.ntruss.com/nmt/v1/translation", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -58,17 +61,15 @@ module.exports = {
                 })
             });
 
-            const data = await response.json();
+            const datas = await trans.json();
 
-            //console.log(data);
+            const translated = datas.message.result.translatedText;
 
-            const translated = data.message.result.translatedText;
-
-            await interaction.reply(`[${src}]${text} >> [${dest}]${translated}`);
+            await message.channel.send(`${translated}`);
 
         } catch(error) {
             console.error(error);
-            await interaction.reply('Error');
+            await message.reply("Error");
         }
     },
 };

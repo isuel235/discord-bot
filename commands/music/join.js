00c719cf2f7d34273f38649a2eb7fceb09e53d12
isuel235 <min1028004@gmail.com>
2026-03-23 14:21:18 +0900

    fix join.js

--- commands/music/join.js
+++ commands/music/join.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,13 +12,23 @@ module.exports = {
         if(!channel) {
             return interaction.reply("You are not in voice channel");
         }
+        
+        const existingConnection = getVoiceConnection(interaction.guild.id);
 
-        const connection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: interaction.guild.id,
-            adapterCreator: interaction.guild.voiceAdapterCreator,
-        });
+        if(existingConnection) {
+            return interaction.reply("Already connected to a voice channel.");
+        }
 
-        await interaction.reply("Joined into voice channel.");
+        try{
+            const connection = joinVoiceChannel({
+                channelId: channel.id,
+                guildId: interaction.guild.id,
+                adapterCreator: interaction.guild.voiceAdapterCreator,
+            });
+
+            await interaction.reply("Joined into voice channel.");
+        } catch(err) {
+            await interaction.reply(err);
+        }
     }
 }
